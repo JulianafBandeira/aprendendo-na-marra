e5f665f625a43f218df19dcb815142e746d65f5d
julianafBandeira <julianafbbarbosa@gmail.com>
2021-01-31 14:41:58 -0300

    projeto completo

---
+++ aprendendo_na_marra/src/main/java/file/java.java
@@ -0,0 +1,20 @@
+/*
+ *
+ */
+package file;
+
+/**
+ *
+ * @author Juliana
+ */
+public class java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World");
+    }
+    
+}
